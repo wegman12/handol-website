488015f871de376f9e2310e873f1fc4a369fa2fc
Kevin Wegman <kevinwegman1993@gmail.com>
2025-03-15 07:21:46 -0400

    Change some styling, create docker.

--- src/components/ApplicationFooter.vue
+++ src/components/ApplicationFooter.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-  <q-footer class="application-footer bg-grey-3 text-black text-subtitle1">
-    <a href="https://maps.app.goo.gl/MYsLshXJ9B6jrGWC9" target="_blank"
-      >3102 Hayes Ave, Sandusky, OH 44870</a
-    >
+  <q-footer class="application-footer bg-grey-8 text-black text-subtitle1">
+    <q-btn text-color="white" flat href="https://maps.app.goo.gl/MYsLshXJ9B6jrGWC9" target="_blank">
+      3102 Hayes Ave, Sandusky, OH 44870
+    </q-btn>
   </q-footer>
 </template>
 
--- src/components/ApplicationHeader.vue
+++ src/components/ApplicationHeader.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <q-header elevated>
+  <q-header class="row application-header justify-center" elevated>
     <q-toolbar>
       <q-toolbar-title> Handol Korean Methodist Church </q-toolbar-title>
     </q-toolbar>
   </q-header>
 </template>
+
+<style lang="scss">
+.application-header {
+  height: 80px;
+}
+</style>
