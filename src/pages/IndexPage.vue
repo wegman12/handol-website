ee0c2e91f538f631cfb74c730207de5dce72288b
Kevin Wegman <kevinwegman1993@gmail.com>
2025-03-15 09:24:35 -0400

    Update home page to include image

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,8 @@
 <template>
-  <q-page class="column items-center q-pa-xl">
-    <div class="row text-h1 q-my-xl text-primary">All are welcome</div>
-    <div class="row q-my-xl">
-      <div style="max-width: 500px">
+  <q-page class="column items-center">
+    <div class="home-page column q-pa-xl items-center">
+      <div class="row text-h2 q-my-xl text-primary">All are welcome!</div>
+      <div class="row q-my-xl text-body1" style="max-width: 800px">
         <p>
           Handol Korean Methodist Church is a religious organization based in Sandusky, OH,
           dedicated to providing spiritual guidance and community support to its members.
@@ -13,8 +13,20 @@
           and connection.
         </p>
       </div>
+      <div class="row q-my-lg text-body1 text-italic">
+        Church services are held on Friday at 5:00pm and Sunday at 11:00am.
+      </div>
     </div>
   </q-page>
 </template>
 
 <script setup lang="ts"></script>
+
+<style lang="scss">
+.home-page {
+  background-color: rgba(168, 168, 168, 0.8);
+  max-width: 1000px;
+  min-height: calc(100vh - 120px);
+  align-self: center;
+}
+</style>
